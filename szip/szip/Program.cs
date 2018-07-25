d561d4121329d53fe4ed9ed4d1ce789042f79508
while <qingfeng346@fengyuezhu.com>
2018-07-25 22:03:59 +0800

    first

---
+++ szip/szip/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace szip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.Compression.
+            GZipStream stream;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
