dbe89c60375e0e27e2bd31c857c13d9b78aece05
linyuan.yang <linyuan.yang@centurygame.com>
2019-01-03 15:14:03 +0800

    升级版本号到 1.0.1

--- szip/src/Version.cs
+++ szip/src/Version.cs
@@ -1,6 +1,6 @@
 namespace szip {
     public static class Version {
-        public const string version = "1.0.2";
+        public const string version = "1.0.1";
         public const string date = "2019-01-03";
     }
 }
