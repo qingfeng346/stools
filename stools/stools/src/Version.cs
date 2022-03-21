48bc4ef96fb5cc3cc8d414c8c85f04ed71ab6fea
linyuan.yang <linyuan.yang@centurygame.com>
2022-03-21 16:40:19 +0800

    version

--- stools/stools/src/Version.cs
+++ stools/stools/src/Version.cs
@@ -1,6 +1,6 @@
 namespace Scorpio.stools {
     public static class Version {
         public const string version = "1.0.0";
-        public const string date = "2022-03-19";
+        public const string date = "2022-03-21";
     }
 }
