c3e5200f2ca8db318054ce5d3c4965db445155f8
while <qingfeng346@fengyuezhu.com>
2022-06-06 11:28:31 +0800

    升级 library

--- stools/stools/src/Version.cs
+++ stools/stools/src/Version.cs
@@ -1,6 +1,6 @@
 namespace Scorpio.stools {
     public static class Version {
         public const string version = "1.0.1";
-        public const string date = "2022-05-29";
+        public const string date = "2022-06-06";
     }
 }
