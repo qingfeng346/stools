b139697e11d571f4779d1c1e42ef4ab1b751c5e9
linyuan.yang <linyuan.yang@centurygame.com>
2022-07-07 18:59:17 +0800

    http 禁止缓存

--- stools/stools/src/HttpUtil.cs
+++ stools/stools/src/HttpUtil.cs
@@ -3,54 +3,62 @@ using System.IO;
 using System.Net.Http;
 using Scorpio.Commons;
 using System.Net;
+using System.Net.Http.Headers;
 using System;
 public class HttpUtil {
     private const int READ_LENGTH = 8192;
     public static async Task<string> Get(string url, Action<HttpRequestMessage> preRequest = null) {
-        var handler = new HttpClientHandler();
-        handler.AllowAutoRedirect = true;
-        handler.UseCookies = false;
-        handler.Credentials = CredentialCache.DefaultCredentials;
-        handler.ClientCertificateOptions = ClientCertificateOption.Automatic;
-        var client = new HttpClient(handler);
-        var message = new HttpRequestMessage(HttpMethod.Get, url);
-        preRequest?.Invoke(message);
+        using (var handler = new HttpClientHandler()) {
+            handler.AllowAutoRedirect = true;
+            handler.UseCookies = false;
+            handler.Credentials = CredentialCache.DefaultCredentials;
+            handler.ClientCertificateOptions = ClientCertificateOption.Automatic;
+            using (var client = new HttpClient(handler)) {
+                var cacheControl = new CacheControlHeaderValue();
+                cacheControl.NoCache = true;
+                cacheControl.NoStore = true;
+                client.DefaultRequestHeaders.CacheControl = cacheControl;
+                var message = new HttpRequestMessage(HttpMethod.Get, url);
+                preRequest?.Invoke(message);
 #if DEBUG
-        Console.WriteLine($"Request Url : {url}");
+                Console.WriteLine($"Request Url : {url}");
 #endif
-        var response = await client.SendAsync(message);
-        return await response.Content.ReadAsStringAsync();
+                var response = await client.SendAsync(message);
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
     }
     public static async Task<long> Download(string url, string file) {
         return await Download(url, file, true, false);
     }
     public static async Task<long> Download(string url, string file, bool progress, bool checkSize) {
-        var client = new HttpClient();
-        var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
-        var bytes = new byte[READ_LENGTH];
-        using (var responseStream = await response.Content.ReadAsStreamAsync()) {
-            long? contentLength = response.Content.Headers?.ContentLength;
-            long readed = 0;
-            long update = DateTime.UtcNow.Ticks;
-            if (checkSize && contentLength != null && File.Exists(file) && new FileInfo(file).Length == contentLength) {
-                return contentLength ?? 0;
-            }
-            long length = contentLength ?? 0;
-            FileUtil.DeleteFile(file);
-            using (var fileStream = new FileStream(file, FileMode.Create)) {
-                while (true) {
-                    var size = await responseStream.ReadAsync(bytes, 0, READ_LENGTH);
-                    if (size <= 0) { break; }
-                    readed += size;
-                    fileStream.Write(bytes, 0, size);
-                    long now = DateTime.UtcNow.Ticks;
-                    if (now - update > 20000000 && progress) {
-                        update = now;
-                        Console.WriteLine($"{file} 进度: {readed.GetMemory()}/{length.GetMemory()}");
+        using (var client = new HttpClient()) {
+            var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+            var bytes = new byte[READ_LENGTH];
+            using (var responseStream = await response.Content.ReadAsStreamAsync()) {
+                long? contentLength = response.Content.Headers?.ContentLength;
+                long readed = 0;
+                long update = DateTime.UtcNow.Ticks;
+                if (checkSize && contentLength != null && File.Exists(file) && new FileInfo(file).Length == contentLength) {
+                    return contentLength ?? 0;
+                }
+                long length = contentLength ?? 0;
+                FileUtil.DeleteFile(file);
+                using (var fileStream = new FileStream(file, FileMode.Create)) {
+                    while (true) {
+                        var size = await responseStream.ReadAsync(bytes, 0, READ_LENGTH);
+                        if (size <= 0) { break; }
+                        readed += size;
+                        fileStream.Write(bytes, 0, size);
+                        long now = DateTime.UtcNow.Ticks;
+                        if (now - update > 20000000 && progress) {
+                            update = now;
+                            Console.WriteLine($"{file} 进度: {readed.GetMemory()}/{length.GetMemory()}");
+                        }
                     }
                 }
+                return readed;
             }
-            return readed;
-        }
+        } 
     }
 }
