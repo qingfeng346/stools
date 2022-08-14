57e30d892674c2cdc7e3cf6cbd150ae13cae38df
while <qingfeng346@126.com>
2022-08-14 21:15:00 +0800

    1.0.2

--- stools/stools/src/Version.cs
+++ stools/stools/src/Version.cs
@@ -1,6 +1,6 @@
 namespace Scorpio.stools {
     public static class Version {
         public const string version = "1.0.2";
-        public const string date = "2022-07-17";
+        public const string date = "2022-08-14";
     }
 }
