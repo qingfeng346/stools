23c0590aac05ab1f8c5c281792defa2d4f6cba6d
linyuan.yang <linyuan.yang@centurygame.com>
2023-03-27 11:49:51 +0800

    u

--- m3u8-downloader/build-release.ps1
+++ m3u8-downloader/build-release.ps1
@@ -8,12 +8,15 @@ echo "start build darwin/amd64 ..."
 go env -w CGO_ENABLED=0 GOOS=darwin GOARCH=amd64
 go build -o ./Releases/m3u8-darwin-amd64 m3u8-downloader.go
 
-# # 【linux/amd64】
-# echo "start build linux/amd64 ..."
-# CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -o ./Releases/m3u8-linux-amd64 m3u8-downloader.go
+# 【linux/amd64】
+echo "start build linux/amd64 ..."
+go env -w CGO_ENABLED=0 GOOS=linux GOARCH=amd64
+go build -o ./Releases/m3u8-linux-amd64 m3u8-downloader.go
 
-# # 【windows/amd64】
-# echo "start build windows/amd64 ..."
-# CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build  -o ./Releases/m3u8-windows-amd64.exe m3u8-downloader.go
+
+# 【windows/amd64】
+echo "start build windows/amd64 ..."
+go env -w CGO_ENABLED=0 GOOS=windows GOARCH=amd64
+go build -o ./Releases/m3u8-windows-amd64.exe m3u8-downloader.go
 
 # echo "Congratulations,all build success!!!"
