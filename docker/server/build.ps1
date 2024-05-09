83e8e502e8e1a723bdfa66024b7b6f8bb41fdb92
linyuan.yang <linyuan.yang@centurygame.com>
2024-05-09 14:04:25 +0800

    build

--- docker/server/build.ps1
+++
@@ -1,5 +0,0 @@
-Remove-Item -Path "./stools" -Force -Recurse
-Copy-Item -Path "../../stools/stools/src/" -Destination "./stools/src/" -Recurse -Force
-Copy-Item -Path "../../stools/stools/stools.csproj" -Destination "./stools/stools.csproj" -Force
-docker build -t stools .
-docker save -o ../stools.tar stools
