2ad15c6b6c937760153045fb7b106f9782816435
linyuan.yang <linyuan.yang@centurygame.com>
2024-05-09 19:11:27 +0800

    fix

--- docker/client/vite.config.js
+++ docker/client/vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  base: "client",
   build: {
     emptyOutDir: true,
     outDir: "../server/client/"
