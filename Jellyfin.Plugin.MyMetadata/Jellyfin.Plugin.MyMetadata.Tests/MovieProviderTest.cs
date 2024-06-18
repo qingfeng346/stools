852ec563a02c2625141f19c354e0d9e418ba9e10
linyuan.yang <linyuan.yang@centurygame.com>
2024-06-18 20:14:41 +0800

    up

---
+++ Jellyfin.Plugin.MyMetadata/Jellyfin.Plugin.MyMetadata.Tests/MovieProviderTest.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Jellyfin.Plugin.MyMetadata.Service.Test;
+
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Entities;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Jellyfin.Plugin.MyMetadata.Tests
+{
+    public class MovieProviderTest
+    {
+        private readonly MovieProvider _provider;
+        public MovieProviderTest(ITestOutputHelper output)
+        {
+            var serviceProvider = ServiceUtils.BuildServiceProvider<MovieProvider>(output);
+            _provider = serviceProvider.GetService<MovieProvider>();
+        }
+
+        [Fact]
+        public async Task TestGetSearchResults()
+        {
+            // Test 1: search metadata.
+            //MovieInfo info = new MovieInfo()
+            //{
+            //    Name = "蝙蝠侠.黑暗骑士",
+            //};
+
+            //var result = await _provider.GetSearchResults(info, CancellationToken.None);
+            //Assert.NotEmpty(result);
+            //string doubanId = result.FirstOrDefault()?.GetProviderId(BaseProvider.ProviderID);
+            //int? year = result.FirstOrDefault()?.ProductionYear;
+            //Assert.Equal("1851857", doubanId);
+            //Assert.Equal(2008, year);
+
+            //// Test 2: Already has provider Id.
+            //info.SetProviderId(BaseProvider.ProviderID, "1851857");
+            //result = await _provider.GetSearchResults(info, CancellationToken.None);
+            //Assert.Single(result);
+            //doubanId = result.FirstOrDefault()?.GetProviderId(BaseProvider.ProviderID);
+            //year = result.FirstOrDefault()?.ProductionYear;
+            //Assert.Equal("1851857", doubanId);
+            //Assert.Equal(2008, year);
+        }
+
+        [Fact]
+        public async Task TestGetMetadata()
+        {
+            // Test 1: Normal case.
+            MovieInfo info = new MovieInfo()
+            {
+                Name = "Source Code"
+            };
+            var meta = await _provider.GetMetadata(info, CancellationToken.None);
+            //Assert.True(meta.HasMetadata);
+            //Assert.Equal("源代码", meta.Item.Name);
+            //Assert.Equal("3075287", meta.Item.GetProviderId(Config.ProviderID));
+            //Assert.Equal(DateTime.Parse("2011-08-30"), meta.Item.PremiereDate);
+
+            // Test 2: Already has provider Id.
+            //info = new MovieInfo()
+            //{
+            //    Name = "Source Code"
+            //};
+            //info.SetProviderId(Config.ProviderID, "1851857");
+            //meta = await _provider.GetMetadata(info, CancellationToken.None);
+            //Assert.True(meta.HasMetadata);
+            //Assert.Equal("蝙蝠侠：黑暗骑士", meta.Item.Name);
+
+            //// Test 2: Not movie type.
+            //info = new MovieInfo()
+            //{
+            //    Name = "大秦帝国"
+            //};
+            //meta = await _provider.GetMetadata(info, CancellationToken.None);
+            //Assert.False(meta.HasMetadata);
+        }
+    }
+}
---
+++ Jellyfin.Plugin.MyMetadata/Jellyfin.Plugin.MyMetadata.Tests/ServiceUtils.cs
@@ -0,0 +1,29 @@
+﻿using Jellyfin.Plugin.MyMetadata.Service.Test;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using Xunit.Abstractions;
+
+namespace Jellyfin.Plugin.MyMetadata.Tests
+{
+    class ServiceUtils
+    {
+        public static ServiceProvider BuildServiceProvider<T>(ITestOutputHelper output) where T : class
+        {
+            var services = new ServiceCollection()
+                .AddHttpClient()
+                .AddLogging(builder => builder.AddXUnit(output).SetMinimumLevel(LogLevel.Debug))
+                .AddLogging(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Debug))
+                .AddSingleton<T>()
+                .AddSingleton<TestHttpService>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Used For FrodoAndroidClient which can not use typed ILogger.
+            var logger = serviceProvider.GetService<ILogger<T>>();
+            services.AddSingleton<ILogger>(logger);
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
