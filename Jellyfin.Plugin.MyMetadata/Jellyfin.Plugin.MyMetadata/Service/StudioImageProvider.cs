21102a700861efb716c1b6011b6f06fd5a291fc2
linyuan.yang <linyuan.yang@centurygame.com>
2024-06-25 22:47:11 +0800

    up

---
+++ Jellyfin.Plugin.MyMetadata/Jellyfin.Plugin.MyMetadata/Service/StudioImageProvider.cs
@@ -0,0 +1,7 @@
+﻿using MediaBrowser.Controller.Entities;
+using Microsoft.Extensions.Logging;
+namespace Jellyfin.Plugin.MyMetadata.Service {
+    public abstract class StudioImageProvider<T> : BaseImageProvider<StudioImageProvider<T>, T, Studio> where T : HttpService {
+        public StudioImageProvider(ILogger<StudioImageProvider<T>> logger, T httpService) : base(logger, httpService) { }
+    }
+}
