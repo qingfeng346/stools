754c90bd841691141797f264ea7881b0a09e0b0f
linyuan.yang <linyuan.yang@centurygame.com>
2024-06-27 00:49:14 +0800

    up

--- movie/client/vite.config.js
+++ movie/client/vite.config.js
@@ -12,5 +12,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  base: "client",
+  build: {
+    emptyOutDir: true,
+    outDir: "../server/client/"
   }
 })
