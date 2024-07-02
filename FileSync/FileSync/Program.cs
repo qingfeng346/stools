15902af1eb4d960c771b8cfc08527583e01682a9
linyuan.yang <linyuan.yang@centurygame.com>
2024-07-02 14:22:14 +0800

    up

--- FileSync/FileSync/Program.cs
+++ FileSync/FileSync/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Scorpio.Commons;
-using System.Collections.Concurrent;
 namespace FileSync {
     using static FileUtil;
     internal class Program {
@@ -20,24 +19,33 @@ namespace FileSync {
                                [ParamterInfo(Label = "目标目录", Param = ParameterTarget, Required = true)] string target) {
             source = Path.GetFullPath(source);
             target = Path.GetFullPath(target);
-            var changeFiles = new ConcurrentQueue<(string, string, WatcherChangeTypes)>();
+            var sync = new object();
+            var changeFiles = new LinkedList<(string, string, WatcherChangeTypes)>();
             var watcher = new FileSystemWatcher(source);
             watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.Size | NotifyFilters.LastWrite | NotifyFilters.CreationTime;
             watcher.Changed += (sender, args) => {
                 logger.info($"文件Changed:{args.FullPath}发生变化,变化类型:{args.ChangeType}");
-                changeFiles.Enqueue((Path.GetFullPath(args.FullPath), "", args.ChangeType));
+                lock (sync) {
+                    changeFiles.AddLast((Path.GetFullPath(args.FullPath), "", args.ChangeType));
+                }
             };
             watcher.Created += (sender, args) => {
                 logger.info($"文件Created:{args.FullPath}发生变化,变化类型:{args.ChangeType}");
-                changeFiles.Enqueue((Path.GetFullPath(args.FullPath), "", args.ChangeType));
+                lock (sync) {
+                    changeFiles.AddLast((Path.GetFullPath(args.FullPath), "", args.ChangeType));
+                }
             };
             watcher.Deleted += (sender, args) => {
                 logger.info($"文件Deleted:{args.FullPath}发生变化,变化类型:{args.ChangeType}");
-                changeFiles.Enqueue((Path.GetFullPath(args.FullPath), "", args.ChangeType));
+                lock (sync) {
+                    changeFiles.AddLast((Path.GetFullPath(args.FullPath), "", args.ChangeType));
+                }
             };
             watcher.Renamed += (sender, args) => {
                 logger.info($"文件Rename {args.OldFullPath} -> {args.FullPath} 变化类型:{args.ChangeType}");
-                changeFiles.Enqueue((Path.GetFullPath(args.FullPath), Path.GetFullPath(args.OldFullPath), args.ChangeType));
+                lock (sync) {
+                    changeFiles.AddLast((Path.GetFullPath(args.FullPath), Path.GetFullPath(args.OldFullPath), args.ChangeType));
+                }
             };
             watcher.Filter = "*";
             watcher.IncludeSubdirectories = true;
@@ -49,29 +57,35 @@ namespace FileSync {
             var targetLength = PathExist(target) ? GetFiles(target, "*").Count : 0;
             logger.info($"开始监听目录:{source},当前文件总数量:{sourceLength}");
             logger.info($"目标同步目录:{target},当前文件总数量:{targetLength}");
+            var nextCheckTime = DateTime.Now;
             Task.Run(async () => {
-                SyncFolder(source, target, null, true, CompareType.SizeAndModifyTime, NameType.None);
+                try {
+                    SyncFolder(source, target, null, true, CompareType.SizeAndModifyTime, NameType.None);
+                } catch (Exception e) {
+                    throw new Exception($"同步目录失败 : {e}");
+                }
                 while (true) {
                     await Task.Delay(500);
                     while (changeFiles.Count > 0) {
                         await Task.Delay(500);
-                        if (changeFiles.TryDequeue(out (string filePath, string oldFilePath, WatcherChangeTypes changeType) changeFile)) {
+                        lock (sync) {
+                            var (filePath, oldFilePath, changeType) = changeFiles.First();
                             try {
-                                switch (changeFile.changeType) {
+                                switch (changeType) {
                                     case WatcherChangeTypes.Created:
                                     case WatcherChangeTypes.Changed:
-                                        SyncFile(changeFile.filePath, GetTargetFile(changeFile.filePath));
+                                        SyncFile(filePath, GetTargetFile(filePath));
                                         break;
                                     case WatcherChangeTypes.Deleted:
-                                        DeleteFile(GetTargetFile(changeFile.filePath));
+                                        DeleteFile(GetTargetFile(filePath));
                                         break;
                                     case WatcherChangeTypes.Renamed:
-                                        MoveFile(GetTargetFile(changeFile.oldFilePath), GetTargetFile(changeFile.filePath));
+                                        MoveFile(GetTargetFile(oldFilePath), GetTargetFile(filePath));
                                         break;
                                 }
                             } catch (Exception e) {
-                                logger.error($"处理文件出错 FilePath:{changeFile.filePath} OldPath:{changeFile.oldFilePath} ChangeType:{changeFile.changeType} : {e}");
-                                changeFiles.Enqueue(changeFile);
+                                logger.error($"处理文件出错 FilePath:{filePath} OldPath:{oldFilePath} ChangeType:{changeType} : {e}");
+                                changeFiles.AddFirst((filePath, oldFilePath, changeType));
                             }
                         }
                     }
